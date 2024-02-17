f0f3f42f4a48ad92d887e3ed268c56ab7b5e518b
Verdict33 <tvt6.bel@gmail.com>
2024-02-17 14:14:03 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -8,3 +8,15 @@ typedef struct vector {
 
 // Возвращает структуру-дескриптор вектор из n значений
 vector createVector(size_t n);
+
+// Изменяет количество памяти, выделенное под хранение элементов вектора
+void reserve(vector *v, size_t newCapacity);
+
+// Удаляет элементы из контейнера, но не освобождает выделенную память
+void clear(vector *v);
+
+// Освобождает память, выделенную под неиспользуемые элементы
+void shrinkToFit(vector *v);
+
+// Освобождает память, выделенную вектору
+void deleteVector(vector *v);
