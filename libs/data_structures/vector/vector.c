2d2418a6068a756fa2845349a15c3ad124b8569d
Verdict33 <tvt6.bel@gmail.com>
2024-02-24 12:52:30 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -9,8 +9,36 @@ vector createVector(size_t capacity){
     return (vector){malloc(sizeof(int) * capacity), 0, capacity};
 }
 
-void reserve(vector *v, size_t newCapacity){
-    realloc(v->data, newCapacity);
+
+void reserve(vector *v, size_t newCapacity) {
+    if (newCapacity == 0) {
+        free(v->data);
+
+    }else{
+        int *newData = realloc(v->data, newCapacity * sizeof(int));
+
+        if (newData == NULL) {
+            fprintf(stderr, "bad alloc");
+            exit(1);
+        } else {
+            if (newCapacity <= v->size)
+                v->size = newCapacity;
+            free(v->data);
+            v->data = newData;
+            v->capacity = newCapacity;
+        }
+    }
+}
+
+
+
+void clear(vector *v){
+    v->size = 0;
+}
+
+
+void shrinkToFit(vector *v){
+    reserve(v, v->size);
 }
 
 
--- main.c
+++ main.c
@@ -1,41 +1,73 @@
-#include <stdio.h>
 #include "libs/algorithms/array/array.h"
 
-size_t ordered_array_set_union(int *set, size_t n_set, int *subset, size_t n_subset, int *set_result){
-    size_t n_set_result = 0;
+int ordered_array_set_difference(int *set1, size_t n_set1, int *set2, size_t n_set2, int *set_result) {
 
-    for(int i = 0; i < n_set; i++)
-        set_result[n_set_result++] = set[i];
+    int index_set1 = 0, index_set2 = 0, necessary_elements = 0;
 
-    for(int i = 0; i < n_subset; i++){
-        int is_union = 1;
-
-        for(int j = 0; j < n_set_result; j++){
-            if(subset[i] == set_result[j]){
-                is_union = 0;
-                break;
-            }
+    while (index_set1 < n_set1 && index_set2 < n_set2) {
+        if (set1[index_set1] == set2[index_set2]) {
+            index_set1++;
+            index_set2++;
+        } else if (set1[index_set1] < set2[index_set2]) {
+            set_result[necessary_elements++] = set1[index_set1++];
+        } else {
+            index_set2++;
         }
-        if(is_union)
-            set_result[n_set_result++] = subset[i];
     }
+    while (index_set1 < n_set1) {
+        set_result[necessary_elements++] = set1[index_set1++];
+    }
+    return necessary_elements;
+}
 
-    return n_set_result;
+void ordered_array_set_union(const int *set1, int n_set1, const int *set2, int n_set2, int *set_result, int *n_set_result) {
+
+    int index_set1 = 0, index_set2 = 0, necessary_elements = 0;
+
+    while (index_set1 < n_set1 && index_set2 < n_set2) {
+        if (set1[index_set1] < set2[index_set2]) {
+            set_result[necessary_elements++] = set1[index_set1++];
+        } else if (set1[index_set1] > set2[index_set2]) {
+            set_result[necessary_elements++] = set2[index_set2++];
+        } else {
+            set_result[necessary_elements++] = set1[index_set1++];
+            index_set2++;
+        }
+    }
+    while (index_set1 < n_set1) {
+        set_result[necessary_elements++] = set1[index_set1++];
+    }
+    while (index_set2 < n_set2) {
+        set_result[necessary_elements++] = set2[index_set2++];
+    }
+    *n_set_result = necessary_elements;
 }
 
 
+
+int unordered_array_set_symmetric_difference(int *set1, size_t n_set1, int *set2, size_t n_set2, int *result, int *n_result) {
+    int temp_set_1[n_set1 + n_set2];
+    int temp_set_2[n_set1 + n_set2];
+
+    int first_difference = ordered_array_set_difference(set1, n_set1, set2, n_set2, temp_set_1);
+    int second_difference = ordered_array_set_difference(set2, n_set2, set1, n_set1, temp_set_2);
+
+    ordered_array_set_union(temp_set_1,first_difference,temp_set_2,second_difference,result, n_result);
+
+}
+
+
+
 int main() {
-    int n_a, n_b, n_c;
-    scanf("%d %d", &n_a, &n_b);
 
-    int a[n_a];
-    inputArray_(a, n_a);
+    int res_1[6] = {1, 3, 4, 7, 9, 10};
+    int C[5] = {4, 7, 8, 11, 12};
 
-    int b[n_b];
-    inputArray_(b, n_b);
+    int n_res_2;
+    int res_2[n_res_2];
 
-    int c[n_c];
-    n_c = ordered_array_set_union(a, n_a, b, n_b, c);
+    unordered_array_set_symmetric_difference(res_1, 6,C, 5,  res_2, &n_res_2);
 
-    outputArray_(c, n_c);
+    outputArray_(res_2, n_res_2);
+
 }
